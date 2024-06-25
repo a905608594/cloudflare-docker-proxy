062b02e551384e7363d832fa02349111bfa0eaea
a905608594 <61930904+a905608594@users.noreply.github.com>
2024-06-25 19:25:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lecp.top": dockerHub,
+  "quay.lecp.top": "https://quay.io",
+  "gcr.lecp.top": "https://gcr.io",
+  "k8s-gcr.lecp.top": "https://k8s.gcr.io",
+  "k8s.lecp.top": "https://registry.k8s.io",
+  "ghcr.lecp.top": "https://ghcr.io",
+  "cloudsmith.lecp.top": "https://docker.cloudsmith.io",
+  "ecr.lecp.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.lecp.top": dockerHub,
 };
 
 function routeByHosts(host) {
